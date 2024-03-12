e3b2ed19f6c6f4a896fdd65a7dc5296436a99285
suriya216 <sssuriya003@gmail.com>
2024-03-12 15:54:45 +0530

    display todo list area

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -24,9 +24,13 @@ function App() {
         <div>
           <div className='btn-area'> 
             <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`} 
-            onClick={() => setIsCompleteScreen(false)}>Todo</button>
+            onClick={() => setIsCompleteScreen(false)}>
+            Todo
+            </button>
             <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`} 
-            onClick={() => setIsCompleteScreen(true)}>Completed</button>
+            onClick={() => setIsCompleteScreen(true)}>
+            Completed
+            </button>
           </div>
           <div className='todo-list'>
             <div className='todo-list-item'>
