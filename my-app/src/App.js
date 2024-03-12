a96ef691ac5a450941d79643b2854febdb074511
suriya216 <sssuriya003@gmail.com>
2024-03-12 15:18:35 +0530

    App.js competion

---
+++ my-app/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My List</h1>
+      <div className='todo-Wrapper'>
+        <div className='todo-input'>
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input type='text' placeholder='Enter the task'></input>
+          </div>
+          <div className="todo-input-item">
+            <label>Description</label>
+            <input type='text' placeholder='Enter the description'></input>
+          </div>
+          <div className="todo-input-item">
+            <button type='button' className='primaryBtn'>Add</button>
+          </div>
+        </div>
+        <div>
+          <div className='btn-area'> 
+            <button>Todo</button>
+            <button>Completed</button>
+          </div>
+          <div className='todo-list'>
+            <div className='todo-list-item'>
+              <h3>Task1</h3>
+              <p>Description</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
