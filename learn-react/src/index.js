ba7e5a90ab1c31f23501928e1f05bfd39521d92e
suriya216 <sssuriya003@gmail.com>
2024-03-13 11:17:42 +0530

    hello message from app.js

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const myElement = <h1>Hello from index.js!</h1>;
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myElement);
+// const myElement = <h1>Hello from index.js!</h1>;
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+// root.render(myElement);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
