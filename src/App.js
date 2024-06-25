241f245b120b91a5aa889b0793fb58e5d6a81535
suriya216 <sssuriya003@gmail.com>
2024-06-25 15:40:51 +0530

    colorchanging arrow

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import NavBar from "./components/NavBar";
+import ColorChanger from "./components/ColorChanger";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <NavBar />
+      <ColorChanger />
     </>
   );
 }
