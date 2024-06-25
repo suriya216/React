f8472c5f32e3f19c9e9ffeb530b488d231968f67
suriya216 <sssuriya003@gmail.com>
2024-06-25 15:36:39 +0530

    arrow fixing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import Header from "./components/Header";
 import "./App.css";
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Header />
+    <>
+      <NavBar />
+    </>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar(){
+    return(
+        <>
+            <div className="arrow-container">
+                <div className="arrow-mark">
+                    <img src="arrow.svg" alt="arrow-icon" className="arrow-icon"></img>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default NavBar;
