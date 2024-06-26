ef5c9f21530a43768d538b6cac64bed17b73254d
suriya216 <sssuriya003@gmail.com>
2024-06-26 15:08:25 +0530

    Complete color changing

--- src/components/ColorChanger.js
+++ src/components/ColorChanger.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 
-function ColorChanger(){
-    return(
-        <>
-        <div className="arrow">
-            <div className="arrow-container">
-                <div className="arrow-mark">
-                    <img src="arrow.svg" alt="arrow-icon" className="arrow-icon"></img>
-                </div>
-            </div>
-            <div className="revarrow-container">
-                <div className="revarrow-mark">
-                    <img src="revarrow.svg" alt="revarrow-icon" className="revarrow-icon"></img>
-                </div>
-            </div>
+function ColorChanger() {
+  const [isArrowVisible, setIsArrowVisible] = useState(true);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = isArrowVisible ? '#FFFFFF' : '#8E44AD';
+  }, [isArrowVisible]);
+
+  const handleToggle = () => {
+    setIsArrowVisible(!isArrowVisible);
+  };
+
+  return (
+    <div className="arrow">
+      {isArrowVisible ? (
+        <div className="arrow-container">
+          <div className="arrow-mark" >
+            <img src="arrow.svg" alt="arrow-icon" className="arrow-icon" onClick={handleToggle}/>
+          </div>
         </div>
-        </>
-    );
+      ) : (
+        <div className="revarrow-container">
+          <div className="revarrow-mark">
+            <img src="revarrow.svg" alt="revarrow-icon" className="revarrow-icon" onClick={handleToggle}/>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default ColorChanger;
