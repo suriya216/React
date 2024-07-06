62f71c7cc4724ab6804dcacf633f46d33d14b1f5
suriya216 <sssuriya003@gmail.com>
2024-07-06 14:13:46 +0530

    creating menu bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ColorChanger from './components/ColorChanger.js';
+import MenuBar from './components/MenuBar.js';
 
 
 function App() {
   return (
-    <>
+    <div className='mainContainer'>
+      <MenuBar />
       <ColorChanger />
-    </>
+    </div>
   );
 }
 
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -3,7 +3,13 @@ import '../App.css';
 function MenuBar() {
   return(
     <>
-      
+      <div className='menuContainer'>
+        <div className='menuBar'>
+          <div className='homeBar'>
+            <h6>Home</h6>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
