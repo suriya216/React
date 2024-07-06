f5955946447fef918212c1a226cbbfd856986f45
suriya216 <sssuriya003@gmail.com>
2024-07-06 14:31:08 +0530

    menu bar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -8,6 +8,15 @@ function MenuBar() {
           <div className='homeBar'>
             <h6>Home</h6>
           </div>
+          <div className='aboutBar'>
+            <h6>About</h6>
+          </div>
+          <div className='projectBar'>
+            <h6>Project</h6>
+          </div>
+          <div className='contactBar'>
+            <h6>Contact</h6>
+          </div>
         </div>
       </div>
     </>
